65c67b78dbd588db33a1faceb85222495a9193aa
vrmcardoso <vrmcardoso@gmail.com>
2022-11-21 17:04:44 +0000

    fixed error in tables users and breweries

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_160337) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_170352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_160337) do
     t.bigint "users_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["users_id"], name: "index_breweries_on_users_id"
   end
 
@@ -46,8 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_160337) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
-    t.string "brewery_name"
-    t.string "brewery_address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
