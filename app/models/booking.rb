2583328d177ca044c6748ef003a4edd9c429a356
vrmcardoso <vrmcardoso@gmail.com>
2022-11-21 17:53:34 +0000

    created validations and added dates to the bookings model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :brewery
+
+  validates :end_date, comparison: { greater_than: :start_date }
 end
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,9 @@
 class Brewery < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates_length_of :description, :minimum => 6
 end
