2eb0f46551eb5995d2b1950c9800681173795205
vrmcardoso <vrmcardoso@gmail.com>
2022-11-22 10:06:45 +0000

    Added Pundit gem

--- Gemfile
+++ Gemfile
@@ -79,3 +79,4 @@ end
 gem "devise"
 
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'main'
+gem "pundit"
--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -3,8 +3,34 @@ class BreweriesController < ApplicationController
 
   def index
     @breweries = Brewery.all
+    @breweries = policy_scope(Brewery)
   end
 
+  def show
+    authorize @brewery
+  end
 
+  def new
+    @brewery = Brewery.new
+    authorize @brewery
+  end
+
+  def create
+    @brewery = Brewery.new(restaurant_params)
+    @brewery.user = current_user
+    authorize @brewery
+  end
+
+  def edit
+    authorize @brewery
+  end
+
+  def update
+    authorize @brewery
+  end
+
+  def destroy
+    authorize @brewery
+  end
 
 end
