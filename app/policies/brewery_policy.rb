a184822ba3e0490679c85317e910675b8f7b01c2
vrmcardoso <vrmcardoso@gmail.com>
2022-11-22 10:55:40 +0000

    Pundit fix

--- app/policies/brewery_policy.rb
+++ app/policies/brewery_policy.rb
@@ -7,8 +7,8 @@ class BreweryPolicy < ApplicationPolicy
       # scope.where(user: user) # If users can only see their restaurants
       # scope.where("name LIKE 't%'") # If users can only see restaurants starting with `t`
       # ...
-      user.admin? ? scope.all : scope.where(user: user)
     end
+  end
 
     def show?
       true
@@ -27,5 +27,4 @@ class BreweryPolicy < ApplicationPolicy
     def destroy?
       record.user == user
     end
-  end
 end
