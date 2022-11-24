4c525747bd146c6e409be12181fcf57c7225bfe5
vrmcardoso <vrmcardoso@gmail.com>
2022-11-24 15:26:45 +0000

    finished navbar

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="navbar"
 export default class extends Controller {
-  static targets = ["navsearch"]
+  static targets = ["navsearch", "navlinks"]
   connect() {
     console.log("teste")
   }
@@ -10,9 +10,11 @@ export default class extends Controller {
     if (window.scrollY >= 250) {
       this.element.classList.add("navbar-lewagon-white")
       this.navsearchTarget.classList.remove("d-none")
+      this.navlinksTarget.classList.add("nav-link-orange")
     } else {
       this.element.classList.remove("navbar-lewagon-white")
       this.navsearchTarget.classList.add("d-none")
+      this.navlinksTarget.classList.remove("nav-link-orange")
     }
   }
 }
