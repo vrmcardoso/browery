e7cc08fce324f29f7d5754ff0924ab050186d766
miguel silva <miguelvdsilva@gmail.com>
2022-11-25 13:03:11 +0000

    fixbugs

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :brewery
 
   validates :end_date, comparison: { greater_than: :start_date }
-  validates :rating, numericality: { included: 1..5 }
 end
