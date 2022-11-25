62297900b23d73c7517c8ef0e992f2e0ddd44005
Havard Steffensen <havardsteffensen@me.com>
2022-11-25 14:58:16 +0000

    All the forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
+
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "breweries#home"
-
+  resources :bookings, only: [] do
+    member do
+      patch "rate"
+    end
+  end
   resources :breweries, only: [:home, :index, :new, :create, :edit, :show, :update] do
     resources :bookings, only: [:create]
   end
+  resources :users, only: [:new, :create, :edit, :show, :update]
   get "profile", to: "pages#profile"
-  resources :bookings, only: [] do
-    patch "rate", to: "bookings#rate"
-  end
 end
